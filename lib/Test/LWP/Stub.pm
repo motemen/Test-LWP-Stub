a2fa539c10230ea278bac5e1aed98c5d0bfd88ee
motemen <motemen@gmail.com>
2012-01-26 17:28:21 +0900

    initial commit

---
+++ lib/Test/LWP/Stub.pm
@@ -0,0 +1,34 @@
+package Test::LWP::Stub;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Test::LWP::Stub -
+
+=head1 SYNOPSIS
+
+  use Test::LWP::Stub;
+
+=head1 DESCRIPTION
+
+Test::LWP::Stub is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
